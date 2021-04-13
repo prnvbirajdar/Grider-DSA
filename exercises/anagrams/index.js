40e07ff99bd797e312c94ca3d1f36516dce631d7
prnvbirajdar <prnvbirajdar@gmail.com>
2021-04-13 16:38:16 -0400

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,29 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  stringA.replace(/[^\w]/g, "").toLowerCase();
+  stringB.replace(/[^\w]/g, "").toLowerCase();
+
+  if (stringA.length !== stringB.length) return false;
+
+  let objA = {};
+  let objB = {};
+
+  for (const char of stringA) {
+    objA[char] = objA[char] + 1 || 1;
+  }
+
+  for (const char of stringB) {
+    objB[char] = objB[char] + 1 || 1;
+  }
+
+  for (const char in objA) {
+    if (objA[char] !== objB[char]) return false;
+  }
+
+  return true
+  
+}
 
 module.exports = anagrams;
