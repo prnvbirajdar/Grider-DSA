d19e798ffbe602c678f03d130c13cedf72a8cfa1
prnvbirajdar <prnvbirajdar@gmail.com>
2021-04-13 16:45:47 -0400

    anagrams method 2

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,29 +8,52 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
+// USING OBJECTS
+
 function anagrams(stringA, stringB) {
-  stringA.replace(/[^\w]/g, "").toLowerCase();
-  stringB.replace(/[^\w]/g, "").toLowerCase();
+  // removed symbols and spaces, converted to lowercase
+  // converted to array. arranged serially, joined to form a string
+  // then compared if they're the same
+  let strA = stringA
+    .replace(/[^\w]/g, "")
+    .toLowerCase()
+    .split("")
+    .sort()
+    .join("");
+  let strB = stringB
+    .replace(/[^\w]/g, "")
+    .toLowerCase()
+    .split("")
+    .sort()
+    .join("");
 
-  if (stringA.length !== stringB.length) return false;
-
-  let objA = {};
-  let objB = {};
-
-  for (const char of stringA) {
-    objA[char] = objA[char] + 1 || 1;
-  }
-
-  for (const char of stringB) {
-    objB[char] = objB[char] + 1 || 1;
-  }
-
-  for (const char in objA) {
-    if (objA[char] !== objB[char]) return false;
-  }
-
-  return true
-  
+  return strA === strB;
 }
 
 module.exports = anagrams;
+
+// USING OBJECTS
+
+// function anagrams(stringA, stringB) {
+//     stringA.replace(/[^\w]/g, "").toLowerCase();
+//     stringB.replace(/[^\w]/g, "").toLowerCase();
+
+//     if (stringA.length !== stringB.length) return false;
+
+//     let objA = {};
+//     let objB = {};
+
+//     for (const char of stringA) {
+//       objA[char] = objA[char] + 1 || 1;
+//     }
+
+//     for (const char of stringB) {
+//       objB[char] = objB[char] + 1 || 1;
+//     }
+
+//     for (const char in objA) {
+//       if (objA[char] !== objB[char]) return false;
+//     }
+
+//     return true;
+//   }
