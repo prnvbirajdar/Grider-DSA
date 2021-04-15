f5117a878da5dd77fde2a8effa4d447862f1a029
prnvbirajdar <prnvbirajdar@gmail.com>
2021-04-14 22:56:33 -0400

    capitalize 2

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,10 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+  }
+}
 
 module.exports = steps;
